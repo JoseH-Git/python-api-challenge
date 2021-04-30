eae7810eb46f88c5570231348f71d61ba72f460a
JoseH-Git <joseh.sierra@outlook.com>
2021-04-29 21:58:08 -0500

    VacationPy heat

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "749b01748e4c8a877009fcee018dfc9c"
 
 # Google API Key
 g_key = "AIzaSyC3WSljwKPXs_1zOn_T7eEnVYkd2lwFfxE"
