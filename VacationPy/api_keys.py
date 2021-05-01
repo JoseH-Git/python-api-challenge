ac3cbf514b0ea065f1b515b1bc12c4dea65a17c9
JoseH-Git <joseh.sierra@outlook.com>
2021-04-30 23:10:48 -0500

    Keys update

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "749b01748e4c8a877009fcee018dfc9c"
+weather_api_key = "XXXX"
 
 # Google API Key
-g_key = "AIzaSyC3WSljwKPXs_1zOn_T7eEnVYkd2lwFfxE"
+g_key = "XXXX"
